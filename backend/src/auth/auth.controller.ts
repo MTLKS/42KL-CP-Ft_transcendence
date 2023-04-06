cfd1fb09d21a4817821938f8c4a4d92bfb0aecc5
Sean Chuah <schuah@u91z08s03.42kl.edu.my>
2023-04-06 21:50:04 +0800

    Added API for reading code and returning cookie

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
+	private myArray = new Array();
+
 	@Post()
 	handleData(@Body() data: any): any {
-		console.log(data);
-	
-		const response = { message: 'Received data successfully' };
-		return response;
-	  }
+		return { message: 'Received data successfully' };
+	}
 
 	@Get()
-	getLogin() : string
-	{
+	getLogin() : string {
 		return ("Successfully get");
+	}
+
+	// Receive code from 42 OAuth
+	@Post('code')
+	login(@Body() data: any, @Res( { passthrough: true} ) response: Response): any {
+		this.myArray.push(data);
+		response.cookie('access_token', data.code, {
+			httpOnly: true,
+			secure: true,
+			sameSite: 'strict',
+			maxAge: 60,
+		});
+		return { message: 'Received code successfully, you should have received a Cookie' };
 	}
 }
