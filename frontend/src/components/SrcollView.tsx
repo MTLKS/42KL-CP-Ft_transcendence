38f611d45360100dcc81e999a8be8ed6b316fdad
ijon <ijontan9@gmail.com>
2023-04-08 04:26:16 +0800

    updated terminal page

--- frontend/src/components/SrcollView.tsx
+++ frontend/src/components/SrcollView.tsx
@@ -7,7 +7,7 @@ interface ScrollViewProps {
 function SrcollView(props: ScrollViewProps) {
   const { children } = props;
   return (
-    <div className=' overflow-y-scroll mt-auto scrollbar-hide'>
+    <div className=' overflow-y-scroll scroll-m-0 scroll-p-0 mt-auto scrollbar-hide'>
       {children}
     </div>
   )
--- frontend/src/pages/Terminal.tsx
+++ frontend/src/pages/Terminal.tsx
@@ -8,16 +8,17 @@ import rickroll from '../functions/rickroll'
 function Terminal() {
   const [elements, setElements] = React.useState([] as JSX.Element[])
   const [index, setIndex] = React.useState(0);
-  function card(index: number) {
-    return <Card key={index}>
-      <p className='text-gray-300 text-2xl tracking-tighter mb-5 text-center h-15'>This is a card</p>
-    </Card>;
-  }
+
 
   return (
     <div className='h-screen flex flex-col'>
       <SrcollView>
-        <p className='text-gray-300 text-2xl tracking-tighter mb-5 text-center h-15'>Welcome to PONGsh</p>
+        <div className=' flex justify-center'>
+          <p className=' glitch text-gray-300 text-2xl tracking-tighter mb-5 h-15'>
+            <span className='glitch-child1'>Welcome to PONGsh</span>
+            Welcome to PONGsh
+          </p>
+        </div>
         {elements}
       </SrcollView>
       <PromptField handleCommands={(command) => {
@@ -41,12 +42,41 @@ function Terminal() {
             setIndex(0);
             setElements([]);
             break;
+          case "help":
+            const newHelpCard = helpCard();
+            const newHelpList = elements.concat(newHelpCard);
+            setIndex(index + 1);
+            setElements(newHelpList);
+            break;
           default:
             break;
         }
       }} />
     </div>
   )
+
+  function card(index: number) {
+    return <Card key={index}>
+      <p className='text-gray-300 text-2xl tracking-tighter mb-5 h-15'>This is a card</p>
+    </Card>;
+  }
+
+  function helpCard() {
+    return <Card key={index}>
+      <p className='text-gray-300 text-1xl tracking-tighter mb-5 h-15 whitespace-pre'>
+        <span className=' text-2xl text-green-600 font-bold'>Get some help!</span><br />
+        add:         add a card <br />
+        clear:       clear the screen <br />
+        cowsay:      make a cow say something <br />
+        help:        show this help message <br />
+        leaderboard: show the leaderboard <br />
+        login:       login to your account <br />
+        ls:          list files <br />
+        ok:          ok <br />
+        sudo:        give you admin privilige <br />
+      </p>
+    </Card>;
+  }
 }
 
 export default Terminal
