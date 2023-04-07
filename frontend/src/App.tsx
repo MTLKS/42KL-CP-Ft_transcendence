acd4314b75f9a1490af6a5421428cf551e999d87
Ricky0625 <rickywong0625@gmail.com>
2023-04-07 23:25:14 +0800

    refactored get code function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,12 @@ import PromptField from "./PromptField/PromptField";
 import Button from "./components/Buttons";
 import Title from "./components/Title";
 import "./index.css";
-import Login from "./login/Login";
 
 function App() {
   newFunction();
   return (
     <div className='bg-gray-900 h-screen w-screen font-'>
       <Title />
-      <Button />
       <PromptField />
     </div>
   )
@@ -17,8 +15,7 @@ function App() {
   function newFunction() {
     const queryString: string = window.location.search;
     const urlParams: URLSearchParams = new URLSearchParams(queryString);
-    const code: any = JSON.parse(`{ "code" : "${urlParams.get('code')}" }`);
-
+    let code: { code: string | null } = { code: urlParams.get('code') };
     if (code.code !== "null")
       console.log(code);
   }
