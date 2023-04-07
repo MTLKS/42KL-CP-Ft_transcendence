aa2edcfe0bb6707257f9cc8c52d2f92b284bc7bc
Ze Hao Ah <zah@u91z06s01.42kl.edu.my>
2023-04-07 18:46:01 +0800

    Able to get access token

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,20 +1,52 @@
-import { Controller, Get, Post, Body, Res, Headers } from '@nestjs/common';
+import { Controller, Get, Post, Body, Req, Res, Headers, Redirect, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Response } from 'express';
+import { query, Response, Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
-
+	
 	@Post()
 	handleData(@Body() data: any): any {
 		return this.authService.handleData(data);
 	}
 
 	/* TEMP REQUEST */
+	// @Get()
+	// getLogin(@Headers() headers: any) : string {
+	// 	this.authService.getLogin();
+	// 	return this.authService.checkSession(headers);
+	// 	// return {url : this.authService.getRedirectLink, statusCode : 302};
+	// }
+
 	@Get()
-	getLogin(@Headers() headers: any) : string {
-		return this.authService.checkSession(headers);
+	getLogin(@Res() res) :void
+	{
+		//redirect to 42 oauth
+		res.redirect(302, this.authService.getRedirectLink());
+		// this.authService.getLogin();
+		// console.log("End of login");
+	}
+
+	@Get("/redirect")
+	getRedirect(@Query('code') code : string) : string
+	{
+		//get the code from query
+		console.log(code);
+
+		//use the code to get token by sending post request
+		this.authService.getTokenInfo(code);
+
+		//get user with token
+
+		//add user to database
+
+		//create session
+
+		//set cookies
+
+		//redirect back
+		return ("welcome back");
 	}
 
 	// Receive code from 42 OAuth
@@ -22,4 +54,7 @@ export class AuthController {
 	receiveCode(@Body() data: any, @Res( { passthrough: true} ) response: Response): any {
 		return this.authService.receiveCode(data, response);
 	}
+
 }
+
+
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -18,9 +18,22 @@ export class AuthService {
 		return { message: "Received data successfully" };
 	}
 
-	getLogin(): string {
-		return "Successfully get login";
+	getLogin() : void {
+
+		//Redirect to the 42 oauth
+		
+		console.log("sucessfully login");
+		console.log(this.getRedirectLink())
+		//Get access token
+
+
+		//Get user data with token
+
+
+		//Check user already has account
 	}
+
+	
 
 	receiveCode(@Body() data: any, @Res() response: Response): any {
 		if (this.myArray.includes(data.code) == false) {
@@ -33,5 +46,41 @@ export class AuthService {
 			maxAge: 60,
 		});
 		return this.myArray;
+	}
+
+	// async getAccessToken(code)
+
+	//Get the link to the 42oauth
+	getRedirectLink():string
+	{
+		const link = "https://api.intra.42.fr/oauth/authorize/";
+		const clientID = 
+		"u-s4t2ud-f4e9a7e43d31ba873132848534cb2ce3c8208fe4ae57e8be9b3f270a17c4f350";
+		const redirect_uri = "http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fredirect"
+
+		return (link + "?client_id=" + clientID + "&redirect_uri=" + redirect_uri + "&response_type=code");
+	}
+
+	//Use the code from query to get token info
+	async getTokenInfo(code : string) : Promise<any>
+	{
+		const data = {
+			"grant_type" : "authorization_code",
+			"client_id" : "u-s4t2ud-f4e9a7e43d31ba873132848534cb2ce3c8208fe4ae57e8be9b3f270a17c4f350",
+			"client_secret" : "secret",
+			"code" : code,
+			"redirect_uri" : "http://localhost:3000/auth/redirect"
+		};
+
+		const response = await fetch("https://api.intra.42.fr/oauth/token", {
+			method : 'POST',
+			headers :{
+				'Content-Type': 'application/json',
+			},
+			body : JSON.stringify(data),
+		});
+		const responseData = await response.json();
+		console.log(responseData);
+    	return responseData;
 	}
 }
