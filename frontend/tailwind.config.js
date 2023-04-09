4a01aaf606141bd408d9be01b47309a6afbb05f2
ijon <ijontan9@gmail.com>
2023-04-09 23:01:50 +0800

    new colors and font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#fef8e2",
+        background: "#242424",
+        redOld: "#ad6454",
+        cyanOld: "#5f928f",
+        greenOld: "#00ff00",
+        yellowOld: "#b3a183",
+        blueOld: "#313440",
+      },
+    },
   },
   plugins: [],
-}
-
+};
