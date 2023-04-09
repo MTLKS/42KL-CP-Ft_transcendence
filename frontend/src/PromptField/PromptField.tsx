5de4e4ef9a0a11ccfd33ef0baec630ca6d8747f6
ijon <ijontan9@gmail.com>
2023-04-09 23:04:09 +0800

    updated PromptField  to be reuseable and align center function

--- frontend/src/PromptField/PromptField.tsx
+++ frontend/src/PromptField/PromptField.tsx
@@ -10,52 +10,49 @@ interface offset {
 
 interface PromptFieldProps {
   handleCommands: (command: string) => void;
+  center: boolean;
+  availableCommands: string[];
+  commandHighLightColor?: string | null;
+  focusColor?: string | null;
+  capitalize?: boolean;
 }
 
 function PromptField(props: PromptFieldProps) {
+  const { handleCommands, center, availableCommands, commandHighLightColor, focusColor, capitalize } = props;
+  const caretStart = center ? 300 - 13.23 / 2 : 16;
+  const fontWidth: number = center ? 13.23 / 2 : 13.23;
   const [value, setValue] = useState('');
-  const [offset, setOffset] = useState({ top: 0, left: 16 } as offset);
+  const [offset, setOffset] = useState({ top: 0, left: caretStart } as offset);
   const [focus, setFocus] = useState(false);
   const [commandHistory, setCommandHistory] = useState([] as string[]);
   const [historyIndex, setHistoryIndex] = useState(-1);
 
   const firstWord: string = value.split(" ")[0];
   const theRest: string = value.split(" ").slice(1).join(" ");
-  const fontWidth: number = 13.23;
   let color: string = "white";
-  switch (firstWord) {
-    case "help":
-    case "login":
-    case "exit":
-    case "sudo":
-    case "leaderboard":
-    case "ok":
-    case "cowsay":
-    case "clear":
-    case "ls":
-    case "add":
-      color = "neonText-cyan transition-all";
-      break;
-    default:
-      color = "neonText-red transition-all";
-      break;
-  }
+  availableCommands.forEach((command) => {
+    if (firstWord === command) {
+      color = commandHighLightColor ?? "white";
+    }
+  });
 
   return (
-    <div className='mx-16'>
-      <div className=' relative bg-slate-600 
-      neonText-white text-2xl tracking-tighter whitespace-pre
-      mb-5 border-4 
-      px-4 rounded-xl h-15'
-        style={{ borderColor: focus ? "cyan" : "white", transition: "border-color 0.5s" }}
+    <div className={center ? 'mx-auto w-[600px]' : 'mx-16'}
+    >
+      <div className=' relative
+      text-white text-2xl tracking-tighter whitespace-pre
+      mb-5 border-4 py-2 mx-auto
+      px-4 rounded-md h-15 pt-3 pb-2'
+        style={{ borderColor: focus ? focusColor ?? "white" : "white", transition: "border-color 0.5s", textAlign: center ? "center" : "left" }}
         onClick={() => { document.querySelector('input')?.focus() }}
       >
         <span className={color} >{firstWord + " "}</span>
         {theRest}
         <input className=' w-0 outline-none bg-transparent text-transparent'
           onInput={(e) => {
-            setValue(e.currentTarget.value);
-            animateCaret({ top: 0, left: 16 + (e.currentTarget.selectionStart!) * fontWidth });
+            if (capitalize ?? false) setValue(e.currentTarget.value.toUpperCase());
+            else setValue(e.currentTarget.value);
+            animateCaret({ top: 0, left: caretStart + (e.currentTarget.selectionStart!) * fontWidth });
           }}
           onClick={(e) => e.stopPropagation()}
           onKeyDown={onKeyDown}
@@ -63,8 +60,8 @@ function PromptField(props: PromptFieldProps) {
           onBlur={() => setFocus(false)}
           onClickCapture={(e) => e.stopPropagation()}
         />
-        <div className={' animate-pulse absolute w-4 h-8 bg-white '}
-          style={{ top: offset.top, left: offset.left }} />
+        <div className={' animate-pulse absolute w-2 h-9 bg-white top-2'}
+          style={{ left: offset.left }} />
       </div>
     </div>
   )
@@ -89,7 +86,7 @@ function PromptField(props: PromptFieldProps) {
   function onKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === 'Enter') {
       e.currentTarget.value = '';
-      props.handleCommands(firstWord);
+      handleCommands(firstWord);
       let newHistory: string[];
       if (commandHistory.length > 20) newHistory = commandHistory.slice(1);
       else if (commandHistory[commandHistory.length - 1] !== firstWord) newHistory = commandHistory.concat(firstWord);
@@ -97,15 +94,15 @@ function PromptField(props: PromptFieldProps) {
       setCommandHistory(newHistory);
       setValue('');
       setHistoryIndex(-1);
-      animateCaret({ top: 0, left: 16 });
+      animateCaret({ top: 0, left: caretStart });
     }
     if (e.key === 'ArrowLeft') {
       if (e.currentTarget.selectionStart! + 1 == 1) return;
-      animateCaret({ top: 0, left: 16 + (e.currentTarget.selectionStart! - 1) * fontWidth });
+      animateCaret({ top: 0, left: caretStart + (e.currentTarget.selectionStart! - 1) * fontWidth });
     }
     if (e.key === 'ArrowRight') {
       if (e.currentTarget.selectionStart! + 1 > value.length) return;
-      animateCaret({ top: 0, left: 16 + (e.currentTarget.selectionStart! + 1) * fontWidth });
+      animateCaret({ top: 0, left: caretStart + (e.currentTarget.selectionStart! + 1) * fontWidth });
     }
     if (e.key === 'ArrowUp') {
       let newHistoryIndex = historyIndex;
@@ -119,7 +116,7 @@ function PromptField(props: PromptFieldProps) {
       e.currentTarget.selectionStart = newCommand.length;
       setValue(newCommand);
       setHistoryIndex(newHistoryIndex)
-      animateCaret({ top: 0, left: 16 + newCommand.length * fontWidth });
+      animateCaret({ top: 0, left: caretStart + newCommand.length * fontWidth });
     }
     if (e.key === 'ArrowDown') {
       let newHistoryIndex = historyIndex;
@@ -129,7 +126,7 @@ function PromptField(props: PromptFieldProps) {
         newHistoryIndex = -1;
         e.currentTarget.value = '';
         setValue('');
-        animateCaret({ top: 0, left: 16 });
+        animateCaret({ top: 0, left: caretStart });
         return;
       } else return;
       const newCommand = commandHistory[historyIndex];
@@ -137,7 +134,7 @@ function PromptField(props: PromptFieldProps) {
       e.currentTarget.selectionStart = newCommand.length;
       setValue(newCommand);
       setHistoryIndex(newHistoryIndex);
-      animateCaret({ top: 0, left: 16 + newCommand.length * fontWidth });
+      animateCaret({ top: 0, left: caretStart + newCommand.length * fontWidth });
     }
   }
 }
