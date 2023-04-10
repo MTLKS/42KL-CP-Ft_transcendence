2fb6c9362c0009988f6412aa36e9a5468478dada
Ricky0625 <rickywong0625@gmail.com>
2023-04-10 11:06:23 +0800

    updated Login.tsx

---
+++ frontend/src/components/Background.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function PolkaDotContainer() {
+  return (
+    <div className="polka-dot"></div>
+  )
+}
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -25,14 +25,14 @@ function Login() {
           style={{ borderWidth: '10px' }}
         >
           <div className='flex flex-rol mt-1'>
-            <div className=' text-3xl'>42</div>
-            <div className=' ml-[0.375rem] mt-[0.375rem] h-[1.5rem] w-16 bg-white rounded-sm' />
+            <div className='text-3xl'>42</div>
+            <div className='ml-[0.375rem] mt-[0.375rem] h-[1.5rem] w-16 bg-white rounded-sm' />
           </div>
           <div className='flex flex-rol'>
-            <div className=' mr-[0.375rem] mt-[0.375rem] h-[1.5rem] w-16 bg-white rounded-sm' />
-            <div className=' text-3xl'>ft</div>
+            <div className='mr-[0.375rem] mt-[0.375rem] h-[1.5rem] w-16 bg-white rounded-sm' />
+            <div className='text-3xl'>ft</div>
           </div>
-          <div className=' text-xs mt-1'>transcendence</div>
+          <div className='text-xs mt-1'>transcendence</div>
         </div>
       </div>
       <PromptField
@@ -41,7 +41,7 @@ function Login() {
         center={true}
         capitalize={true}
       />
-      <div className='text-md'>HAVE YOU TRIED THE 'LOGIN' COMMAND?</div>
+      <div className='text-md uppercase'>have you tried the 'login' command?</div>
     </div >
   )
 
