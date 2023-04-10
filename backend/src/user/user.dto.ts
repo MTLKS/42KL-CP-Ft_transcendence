961616930c73aa1988d5b399c0fa4f7324cc8487
Sean Chuah <schuah@u91z08s03.42kl.edu.my>
2023-04-10 22:50:52 +0800

    Cleanup some API and documentations

--- backend/src/user/user.dto.ts
+++ backend/src/user/user.dto.ts
@@ -1,4 +1,8 @@
 export class UserDTO{
-	intraID : string;
-	intraName : string;
+	intraUrl: string; //url
+	intraName : string; //login
+	intraEmail: string; //email
+	intraImageMedium: string; //image.versions.medium
+	intraImageSmall: string; //image.versions.small
+	intraBlackhole: string; // blackholed_at
 }
