ab9905270703f9d7cfc5fe44d1176fb54a906abb
Ze Hao Ah <zah@u91z06s01.42kl.edu.my>
2023-04-10 15:08:37 +0800

    Able to get user data

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,8 +1,26 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { UserDTO } from "./user.dto";
 
 @Injectable()
 export class UserService {
-	getUserData(): any {
-		return { name: "schuah" };
+	//Use access token to get user info
+	async getUserData(accessToken : string) : Promise<UserDTO>{
+		const HEADER = "Bearer " + accessToken;
+		const RESPONSE = await fetch ("https://api.intra.42.fr/v2/me", {
+			method : "GET",
+			headers : {
+				'Authorization': HEADER,
+			}
+		});
+		if (RESPONSE.ok){
+			const USER_DATA = await RESPONSE.json();
+			const USER_DTO =  new UserDTO();
+			USER_DTO.intraID = USER_DATA.id;
+			USER_DTO.intraName = USER_DATA.login;
+			return (USER_DTO);
+		}
+		else{
+			throw new Error('Authentication failed');
+		}
 	}
 }
