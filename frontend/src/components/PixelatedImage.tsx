edc3ea7e3a73ff00cc839ffeae5b02539e33d0f0
ijon <ijontan9@gmail.com>
2023-04-11 23:06:55 +0800

    pixelatedImage now caches image and crop to square

--- frontend/src/components/PixelatedImage.tsx
+++ frontend/src/components/PixelatedImage.tsx
@@ -7,39 +7,34 @@ interface PixelatedImageProps {
 }
 
 function PixelatedImage(props: PixelatedImageProps) {
-  const [image, setImage] = useState<HTMLImageElement>(new Image());
   const { src, pixelSize = 1, className } = props;
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    cacheImage()
-  }, [src]);
 
   useEffect(() => {
-    draw();
+    saveImageToCache(src).then((imageData) => {
+      const image = new Image();
+      image.src = imageData;
+      draw(image);
+    });
   }, [pixelSize]);
 
   return (
     <canvas id="canvas" ref={canvasRef} className={className} />
   )
-  function cacheImage() {
-    const img = new Image();
-    img.src = src;
-    setImage(img);
-  }
 
-  function draw() {
+
+  function draw(image: HTMLImageElement) {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     image.src = src;
+    image.crossOrigin = "Anonymous";
     // image.src = 'https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*';
     image.onload = () => {
-      canvas.width = image.width;
-      canvas.height = image.height;
       ctx!.imageSmoothingEnabled = false;
       (canvas as any).willReadFrequently = true;
-      ctx!.drawImage(image, 0, 0);
+      cropToSquare(image, canvas, ctx!);
 
       const imageData = ctx!.getImageData(0, 0, canvas.width, canvas.height);
       for (let y = 0; y < canvas.height; y += pixelSize) {
@@ -63,6 +58,37 @@ function PixelatedImage(props: PixelatedImageProps) {
       ctx!.putImageData(imageData, 0, 0);
     }
 
+  }
+
+  function saveImageToCache(imageUrl: string) {
+    const cacheKey = `image-${imageUrl}`;
+    const cachedImageData = sessionStorage.getItem(cacheKey);
+    if (cachedImageData) {
+      // Image data is already cached, no need to download it again
+      return Promise.resolve(cachedImageData);
+    }
+    // Image data is not cached, download it and save to cache
+    return fetch(imageUrl)
+      .then(response => response.blob())
+      .then(blob => new Promise<string>((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          const imageData = reader.result as string;
+          sessionStorage.setItem(cacheKey, imageData);
+          resolve(imageData);
+        };
+        reader.onerror = reject;
+        reader.readAsDataURL(blob);
+      }));
+  }
+
+  function cropToSquare(image: HTMLImageElement, canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    const size = Math.min(image.width, image.height);
+    const x = (image.width - size) / 2;
+    const y = (image.height - size) / 2;
+    canvas.width = size;
+    canvas.height = size;
+    ctx.drawImage(image, x, y, size, size, 0, 0, size, size);
   }
 }
 
