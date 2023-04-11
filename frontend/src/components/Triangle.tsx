532c5d8ca049bd88b7fba892c41e5a2902ab9af4
ijon <ijontan9@gmail.com>
2023-04-11 20:39:46 +0800

    added expanded profile

--- frontend/src/components/Triangle.tsx
+++ frontend/src/components/Triangle.tsx
@@ -5,10 +5,10 @@ interface TriangleProps {
   w?: number
   h?: number
   direction?: 'top' | 'right' | 'bottom' | 'left'
-  color?: 'dimshadow' | 'highlight'
+  color?: 'fill-dimshadow' | 'fill-highlight'
 }
 
-const Triangle = ({ w = 20, h = 20, direction = 'top', color = 'highlight' }: TriangleProps) => {
+const Triangle = ({ w = 20, h = 20, direction = 'top', color = 'fill-highlight' }: TriangleProps) => {
   const points = {
     top: [`${w / 2},0`, `0,${h}`, `${w},${h}`],
     right: [`0,0`, `0,${h}`, `${w},${h / 2}`],
@@ -17,8 +17,8 @@ const Triangle = ({ w = 20, h = 20, direction = 'top', color = 'highlight' }: Tr
   }
 
   return (
-    <svg width={w} height={h} className={'fill-' + color}>
-      <polygon points={points[direction].join(' ')} />
+    <svg width={w} height={h}>
+      <polygon points={points[direction].join(' ')} className={color} />
       Sorry, your browser does not support inline SVG.
     </svg>
   )
