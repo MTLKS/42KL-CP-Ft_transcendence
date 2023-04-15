42124a0155e5f36305fe617b4ec3885f11d2ce8c
Ricky0625 <rickywong0625@gmail.com>
2023-04-15 21:54:00 +0800

    Updated PolkaDotContainer
    - Now can accept:
    	- Array of JSX element
    	- Single JSX element

--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 interface ContainerProps {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
 };
 
 export function PolkaDotContainer(props: ContainerProps) {
