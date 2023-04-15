ca40e8e4e3eb3f9c827a1869e1771aedc2bfbe7c
ijon <itan@student.42kl.edu.my>
2023-04-15 16:20:23 +0800

    trying to fix matrixRain's problem

--- frontend/src/widgets/MatrixRain.tsx
+++ frontend/src/widgets/MatrixRain.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react'
 import sleep from '../functions/sleep';
 
-var tileSize: number = 20;
+var tileWidth: number = 15;
+var tileHeight: number = 10;
+var fontSize: number = 12;
 var maxStackHeight: number;
 
-var fadeFactor = 0.1;
+var fadeFactor = 0.03;
 
 interface column {
   x: number;
@@ -32,10 +34,12 @@ function MatrixRain(props: MatrixRainProps) {
       const { width, height } = container.getBoundingClientRect();
       canvas.width = width;
       canvas.height = height;
+      maxStackHeight = Math.ceil(canvas.height / tileHeight);
+
       columns = [];
-      for (let i = 0; i < canvas.width / tileSize; ++i) {
+      for (let i = 0; i < canvas.width / tileWidth; ++i) {
         var column: column = {
-          x: i * tileSize,
+          x: i * tileWidth,
           stackHeight: 10 + Math.random() * maxStackHeight,
           stackCounter: 0
         };
@@ -59,11 +63,11 @@ function MatrixRain(props: MatrixRainProps) {
     const { width, height } = container.getBoundingClientRect();
     canvas.width = width;
     canvas.height = height;
-    maxStackHeight = Math.ceil(canvas.height / tileSize);
+    maxStackHeight = Math.ceil(canvas.height / tileHeight);
     // divide the canvas into columns
-    for (let i = 0; i < canvas.width / tileSize; ++i) {
+    for (let i = 0; i < canvas.width / tileWidth; ++i) {
       var column: column = {
-        x: i * tileSize,
+        x: i * tileWidth,
         stackHeight: 10 + Math.random() * maxStackHeight,
         stackCounter: 0
       };
@@ -72,7 +76,7 @@ function MatrixRain(props: MatrixRainProps) {
 
 
     requestAnimationFrame(() => draw(ctx!, canvas));
-  }, [animate])
+  }, [])
 
   return (
     <div className='relative w-full h-full flex-1' ref={containerRef}>
@@ -85,16 +89,20 @@ function MatrixRain(props: MatrixRainProps) {
   async function draw(ctx: CanvasRenderingContext2D, canvas: HTMLCanvasElement) {
     {
       // draw a semi transparent black rectangle on top of the scene to slowly fade older characters
-      ctx.fillStyle = `rgba( 36, 36, 36 , ${fadeFactor} )`;
+      // ctx.globalAlpha = 0.95;
+      ctx.fillStyle = `rgba( 0, 0, 0 , ${fadeFactor} )`;
       ctx.fillRect(0, 0, canvas.width, canvas.height);
-
+      // ctx.globalCompositeOperation = 'copy';
+      // ctx.drawImage(canvas, 0, 0);
+      // ctx.globalCompositeOperation = 'source-over';
+      // ctx.globalAlpha = 1;
       // pick a font slightly smaller than the tile size
-      ctx.font = `${tileSize - 10}px jetbrains mono`;
-      ctx.fillStyle = "rgb( 254, 248, 226, 0.4 )";
+      ctx.font = `${fontSize}px jetbrains mono`;
+      ctx.fillStyle = "rgb( 254, 248, 226, 1 )";
       for (let i = 0; i < columns.length; ++i) {
         // pick a random ascii character (change the 94 to a higher number to include more characters)
         var randomCharacter = String.fromCharCode(33 + Math.floor(Math.random() * 94));
-        ctx.fillText(randomCharacter, columns[i].x, columns[i].stackCounter * tileSize + tileSize);
+        ctx.fillText(randomCharacter, columns[i].x, columns[i].stackCounter * tileHeight + tileWidth);
 
         // if the stack is at its height limit, pick a new random height and reset the counter
         if (++columns[i].stackCounter >= columns[i].stackHeight) {
@@ -102,7 +110,7 @@ function MatrixRain(props: MatrixRainProps) {
           columns[i].stackCounter = 0;
         }
       }
-      await sleep(70);
+      await sleep(50);
       requestAnimationFrame(() => draw(ctx, canvas));
     }
   }
