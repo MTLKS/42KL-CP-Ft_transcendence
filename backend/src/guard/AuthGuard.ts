ce7f281835ad38266a95c4484feb01199b6008e2
Sean Chuah <schuah@u91z08s03.42kl.edu.my>
2023-04-20 21:21:34 +0800

    Added more colors YAY

--- backend/src/guard/AuthGuard.ts
+++ backend/src/guard/AuthGuard.ts
@@ -6,6 +6,7 @@ import { Repository } from "typeorm";
 
 const	GREEN	=	"\x1b[32m";
 const	RED		= "\x1b[31m";
+const BLUE = "\x1b[34m";
 const	RESET = "\x1b[0m";
 
 @Injectable()
@@ -17,13 +18,13 @@ export class AuthGuard implements CanActivate
 	async canActivate(context: ExecutionContext): Promise<boolean> {
 		const REQUEST = context.switchToHttp().getRequest();
 		const AUTH_CODE = REQUEST.header('Authorization');
-		console.log("Authorization code:", AUTH_CODE);
+		console.log(BLUE + "Authorization code:", AUTH_CODE + RESET);
 		try {
 			const ACCESS_TOKEN = CryptoJS.AES.decrypt(AUTH_CODE, process.env.ENCRYPT_KEY).toString(CryptoJS.enc.Utf8);
-			console.log("accessToken:", ACCESS_TOKEN);
-			const DATA = await this.userRepository.find({ where: {accessToken: ACCESS_TOKEN} })
-			console.log(DATA.length !== 0 ? GREEN + "Authorization is valid" + RESET : RED + "Authorization is invalid" + RESET);
-			return DATA.length !== 0
+			console.log(BLUE + "accessToken:", ACCESS_TOKEN + RESET);
+			const USER = await this.userRepository.find({ where: {accessToken: ACCESS_TOKEN} })
+			console.log(USER.length !== 0 ? GREEN + USER[0].intraName + " has connected" + RESET : RED + "Authorization is invalid" + RESET);
+			return USER.length !== 0
 		} catch {
 			console.log(RED + "Authorization is invalid" + RESET);
 			return false;
