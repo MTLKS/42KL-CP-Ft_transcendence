25bae6df576f9a43c4ae22ccca138bceb46bac21
Sean Chuah <schuah@u91z08s03.42kl.edu.my>
2023-04-20 21:16:21 +0800

    Added colors to guard

--- backend/src/guard/AuthGuard.ts
+++ backend/src/guard/AuthGuard.ts
@@ -4,6 +4,10 @@ import { User } from "src/entity/user.entity";
 import * as CryptoJS from 'crypto-js';
 import { Repository } from "typeorm";
 
+const	GREEN	=	"\x1b[32m";
+const	RED		= "\x1b[31m";
+const	RESET = "\x1b[0m";
+
 @Injectable()
 export class AuthGuard implements CanActivate
 {
@@ -18,10 +22,10 @@ export class AuthGuard implements CanActivate
 			const ACCESS_TOKEN = CryptoJS.AES.decrypt(AUTH_CODE, process.env.ENCRYPT_KEY).toString(CryptoJS.enc.Utf8);
 			console.log("accessToken:", ACCESS_TOKEN);
 			const DATA = await this.userRepository.find({ where: {accessToken: ACCESS_TOKEN} })
-			console.log("Guard End");
+			console.log(DATA.length !== 0 ? GREEN + "Authorization is valid" + RESET : RED + "Authorization is invalid" + RESET);
 			return DATA.length !== 0
 		} catch {
-			console.log("Authorization is invalid");
+			console.log(RED + "Authorization is invalid" + RESET);
 			return false;
 		}
 	}
