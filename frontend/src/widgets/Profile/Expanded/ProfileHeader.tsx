0d5f1374151522db25de4616cfd2f31acbe54ca0
Ricky0625 <rickywong0625@gmail.com>
2023-04-22 13:50:12 +0800

    added link to intra profile

--- frontend/src/widgets/Profile/Expanded/ProfileHeader.tsx
+++ frontend/src/widgets/Profile/Expanded/ProfileHeader.tsx
@@ -26,7 +26,7 @@ function ProfileHeader(props: ProfileHeaderProps) {
       style={{ height: height }}
     >
       <div className='flex flex-col flex-1 justify-center bg-dimshadow px-5'>
-        <div className=' text-2xl text-highlight font-extrabold'>{userName} <a href='' className='hover:underline cursor-pointer'>({intraName})</a></div>
+        <div className=' text-2xl text-highlight font-extrabold'>{userName} <a href={`https://profile.intra.42.fr/users/${intraName}`} className='hover:underline cursor-pointer' target='_blank'>({intraName})</a></div>
         <div className=' text-xs text-highlight'>THE BLACKHOLE DESTROYER</div>
       </div>
       <div className='flex flex-row w-fit items-center bg-dimshadow p-8'>
