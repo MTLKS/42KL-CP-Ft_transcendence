c27cb4b01274b77904a496f721312d68f2795e6a
MTLKS <matthew.liew99@gmail.com>
2023-04-28 10:46:32 +0000

    Add lobby logic

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -1,87 +1,123 @@
-import { Injectable } from "@nestjs/common";
-import { UserService } from "src/user/user.service";
-import { GameDTO } from "src/dto/game.dto";
-import { Ball } from "./entity/ball";
+import { Injectable, Inject } from '@nestjs/common';
+import { WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { UserService } from 'src/user/user.service';
+import { GameDTO } from 'src/dto/game.dto';
+import { Ball } from './entity/ball';
 
 interface GameData {
-	player1_id: string;
-	player2_id: string;
-	canvasWidth: number;
-	canvasHeight: number;
+  player1_id: string;
+  player2_id: string;
+  canvasWidth: number;
+  canvasHeight: number;
 }
 
 @Injectable()
 export class GameService {
-	constructor(
-		private readonly userService: UserService
-		) {}
+  constructor(private readonly userService: UserService) {}
 
-	private gameState : GameDTO = {
-		ballPosX: 0,
-		ballPosY: 0,
-		leftPaddlePosY: 0,
-		rightPaddlePosY: 0,
-	}
+  queue = [];
+  ingame = [];
 
-	private gameData : GameData = {
-		player1_id: '',
-		player2_id: '',
-		canvasWidth: 100,
-		canvasHeight: 100,
-	}
+  async joinQueue(client: Socket, server: Server) {
+    const USER_DATA = await this.userService.getMyUserData(
+      client.handshake.headers.authorization,
+    );
+    if (USER_DATA.error !== undefined) return;
+    if (this.ingame.find((e) => e.intraName === USER_DATA.intraName)) {
+      console.log(`${USER_DATA.intraName} is already in a game.`);
+      client.emit('error', { error: 'already ingame' });
+      return;
+    }
+    if (this.queue.find((e) => e.intraName === USER_DATA.intraName)) {
+      console.log(`${USER_DATA.intraName} is already in the queue.`);
+      client.emit('error', { error: 'already in queue' });
+      return;
+    }
+    console.log(`${USER_DATA.intraName} joins queue as ${client.id}.`);
+    this.queue.push({ intraName: USER_DATA.intraName, socket: client });
+    console.log(this.queue);
 
-	private PADDLE_SPEED = 5;
-	private BALL = new Ball(this.gameState.ballPosX, this.gameState.ballPosY, 10, 10);
+    if (this.queue.length >= 2) {
+      var player1 = this.queue.pop();
+      this.ingame.push(player1);
+      var player2 = this.queue.pop();
+      this.ingame.push(player2);
 
-	//TODO: generate unique id
-	createGameRoom() : string {
-		return 'room';
-	}
+      this.startGame(player1, player2, server);
+    }
+  }
 
-	async startGame(player1: any, player2: any, body: any, server: any){
-		//Leave lobby room
-		player1.join(this.createGameRoom());
-		player2.join(this.createGameRoom());
+  async leaveQueue(client: Socket) {
+    const USER_DATA = await this.userService.getMyUserData(
+      client.handshake.headers.authorization,
+    );
+    if (USER_DATA.error !== undefined) return;
+    this.queue = this.queue.filter(function (e) {
+      return e.intraName !== USER_DATA.intraName;
+    });
+    console.log(`${USER_DATA.intraName} left queue as ${client.id}.`);
+    console.log(this.queue);
+  }
 
-		//Get both player info
-		// console.log(body.name);
-		// const TEST = this.userService.getUserDataByIntraName(body.name);
-		// console.log(TEST);
+  // const ALL_GAME:
 
-		//SEt game data
+  //TODO: generate unique id
+  createGameRoom(): string {
+    return 'room';
+  }
 
-		//Change both player status to in game
-		this.BALL.initVelocity(5,2);
-		this.gameLoop(server);
-	}
+  async startGame(player1: any, player2: any, server: Server) {
+    console.log(
+      `Game start with ${player1.intraName} and ${player2.intraName}`,
+    );
+    player1.socket.emit('game', `game start: opponent = ${player2.intraName}`);
+    player2.socket.emit('game', `game start: opponent = ${player1.intraName}`);
+    console.log(player1, player2);
 
-	async gameEnd(player1: any, player2: any){
-		player1.leave(this.createGameRoom());
-		player2.leave(this.createGameRoom());
+    //Leave lobby room
+    player1.join(this.createGameRoom());
+    player2.join(this.createGameRoom());
 
-		//TODO : send result to user service
-	}
+    //Get both player info
+    // console.log(body.name);
+    // const TEST = this.userService.getUserDataByIntraName(body.name);
+    // console.log(TEST);
 
-	playerMove(socketId: string, value: number){
-		if (socketId == this.gameData.player1_id){
-			this.gameState.leftPaddlePosY += value;
-		}
-		else if (socketId == this.gameData.player2_id){
-			this.gameState.rightPaddlePosY += value;
-		}
-	}
+    //SEt game data
 
-	gameUpdate(gameState){
-		this.BALL.update();
-		gameState.ballPosX = this.BALL.posX;
-		gameState.ballPosY = this.BALL.posY;
-		this.BALL.checkContraint(gameState.canvasWidth,gameState.canvasHeight);
-	}
+    //Change both player status to in game
+    this.BALL.initVelocity(5, 2);
+    this.gameLoop(server);
+  }
 
-	gameLoop(server: any){
-		setInterval(() => {
-			this.gameUpdate(this.gameState);
-			server.emit('gameLoop', this.gameState);
-		}, 1000/60);
-	}
+  async gameEnd(player1: any, player2: any) {
+    player1.leave(this.createGameRoom());
+    player2.leave(this.createGameRoom());
+
+    //TODO : send result to user service
+    //TODO : remove users from ingame list
+  }
+
+  playerMove(socketId: string, value: number) {
+    if (socketId == this.gameData.player1_id) {
+      this.gameState.leftPaddlePosY += value;
+    } else if (socketId == this.gameData.player2_id) {
+      this.gameState.rightPaddlePosY += value;
+    }
+  }
+
+  gameUpdate(gameState) {
+    this.BALL.update();
+    gameState.ballPosX = this.BALL.posX;
+    gameState.ballPosY = this.BALL.posY;
+    this.BALL.checkContraint(gameState.canvasWidth, gameState.canvasHeight);
+  }
+
+  gameLoop(server: any) {
+    setInterval(() => {
+      this.gameUpdate(this.gameState);
+      server.emit('gameLoop', this.gameState);
+    }, 1000 / 60);
+  }
 }
