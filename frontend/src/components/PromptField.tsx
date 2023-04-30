cc425ea0a6137c55038c4baf162cdaf03e53cd22
ijon <ijontan9@gmail.com>
2023-04-30 23:10:49 +0800

    1. fixed weird cursor position 2. now command history works properly 3. added show tooltips 4. added auto complete with tab

--- frontend/src/components/PromptField.tsx
+++ frontend/src/components/PromptField.tsx
@@ -2,6 +2,7 @@ import React, { Fragment, Ref, createRef, forwardRef, useEffect, useImperativeHa
 import sleep from '../functions/sleep'
 import login from '../functions/login'
 import rickroll from '../functions/rickroll'
+import { flushSync } from 'react-dom';
 
 interface offset {
   top: number,
@@ -11,20 +12,23 @@ interface offset {
 interface PromptFieldProps {
   handleCommands: (commands: string[], fullstring: string) => void;
   center: boolean;
-  availableCommands: string[];
+  availableCommands?: string[];
   commandClassName?: string | null;
   focusColor?: string | null;
   capitalize?: boolean;
   errorClassName?: string | null;
   wrap?: boolean;
+  enableHistory?: boolean;
+  showtip?: boolean;
 }
 
 const PromptField = forwardRef((props: PromptFieldProps, ref) => {
-  const { handleCommands, center, availableCommands, commandClassName, focusColor, capitalize, errorClassName, wrap } = props;
+  const { handleCommands, center, availableCommands = [], commandClassName,
+    focusColor, capitalize, errorClassName, wrap, enableHistory = false, showtip = false } = props;
   const caretStart = center ? 300 - 13.23 / 2 : 4;
   const fontWidth: number = 10;
   const [value, setValue] = useState('');
-  const [offset, setOffset] = useState({ top: 0, left: caretStart } as offset);
+  const [offset, setOffset] = useState({ top: 12, left: caretStart } as offset);
   const [focus, setFocus] = useState(false);
   const [commandHistory, setCommandHistory] = useState([] as string[]);
   const [historyIndex, setHistoryIndex] = useState(-1);
@@ -63,13 +67,17 @@ const PromptField = forwardRef((props: PromptFieldProps, ref) => {
       else
         animateCaret({ top: inputRef.current!.offsetTop, left: inputRef.current!.offsetLeft - fontWidth });
     }
+    inputRef.current!.focus();
   }, [value]);
+
+  const toolTip = availableCommands.find((command) => command.startsWith(value));
+
   return (
     <div className={center ? 'mx-auto w-[600px]' : 'mx-2'}
     >
       <div className=' relative
       text-highlight text-2xl tracking-tighter whitespace-pre
-      mx-auto overflow-hidden flex
+      mx-auto flex
       px-1 rounded-md h-15 pt-3 pb-3'
         style={{
           borderColor: focus ? focusColor ?? "#fef8e2" : "#fef8e2", transition: "border-color 0.5s",
@@ -81,7 +89,7 @@ const PromptField = forwardRef((props: PromptFieldProps, ref) => {
         ref={divRef}
       >
         {spans}
-        <input className=' w-0 outline-none bg-transparent text-transparent'
+        <input className='  w-0 outline-none bg-transparent text-transparent'
           ref={inputRef}
           onInput={(e) => {
 
@@ -97,6 +105,11 @@ const PromptField = forwardRef((props: PromptFieldProps, ref) => {
           onBlur={() => setFocus(false)}
           onClickCapture={(e) => e.stopPropagation()}
         />
+        <div className=' absolute opacity-20'
+          style={{ display: toolTip && value !== "" && showtip ? "" : "none" }}
+        >
+          {toolTip}
+        </div>
         <div className={'animate-pulse absolute w-2 h-9 bg-highlight top-2'}
           style={{ left: offset.left, top: offset.top }} />
       </div>
@@ -110,24 +123,27 @@ const PromptField = forwardRef((props: PromptFieldProps, ref) => {
   function checkValid() {
 
     let index = 0;
-    for (const val in splitValue) {
-
-      const element = val;
+    splitValue.forEach((element) => {
       if (element === "") {
         spansStyles.push('');
-        continue;
+        return;
       }
       if (index == 0) {
-        if (availableCommands.includes(element))
-          spansStyles.push(commandClassName ?? '');
-        else
-          spansStyles.push(errorClassName ?? "underline decoration-3 decoration-accRed");
+        let found = false;
+        availableCommands.forEach((command) => {
+          if (command.startsWith(element)) {
+            spansStyles.push(commandClassName ?? '');
+            found = true;
+            return;
+          }
+        });
+        if (!found) spansStyles.push(errorClassName ?? "underline decoration-3 decoration-accRed");
       }
       else {
         spansStyles.push('');
       }
       index++;
-    }
+    });
   }
 
   function lerpOffset(a: offset, b: offset, t: number) {
@@ -151,47 +167,40 @@ const PromptField = forwardRef((props: PromptFieldProps, ref) => {
     if (e.key === 'Enter') {
       e.currentTarget.value = '';
       handleCommands(words, value);
-      let newHistory: string[];
-      if (commandHistory.length > 20) newHistory = commandHistory.slice(1);
-      else if (commandHistory[commandHistory.length - 1] !== words[0]) newHistory = commandHistory.concat(words[0]);
-      else newHistory = commandHistory;
+      let newHistory = [...commandHistory];
+      if (commandHistory.length > 20) newHistory = newHistory.slice(1);
+      else if (commandHistory[commandHistory.length - 1] !== words[0] && words[0]) newHistory.push(words[0]);
       setCommandHistory(newHistory);
       setValue('');
       setHistoryIndex(-1);
-      animateCaret({ top: 0, left: caretStart });
     }
     if (e.key === 'ArrowRight' || e.key === 'ArrowLeft') e.currentTarget.setSelectionRange(e.currentTarget.value.length, e.currentTarget.value.length);
-    if (e.key === 'ArrowUp') {
+    if ((e.key === 'ArrowUp' || e.key === 'ArrowDown') && enableHistory) {
       let newHistoryIndex = historyIndex;
-      if (commandHistory.length == 0) return;
-      if (historyIndex == -1) newHistoryIndex = commandHistory.length - 1;
-      else if (historyIndex > 0) newHistoryIndex = historyIndex - 1;
-      else return;
-      const newCommand = commandHistory[newHistoryIndex];
-      console.log(historyIndex - 1);
-      e.currentTarget.value = newCommand;
-      e.currentTarget.selectionStart = newCommand.length;
-      setValue(newCommand);
-      setHistoryIndex(newHistoryIndex)
-      animateCaret({ top: 0, left: caretStart + newCommand.length * fontWidth });
-    }
-    if (e.key === 'ArrowDown') {
-      let newHistoryIndex = historyIndex;
-      console.log(newHistoryIndex);
-      if (historyIndex <= commandHistory.length - 1) newHistoryIndex = historyIndex + 1;
-      else if (historyIndex > commandHistory.length - 1) {
-        newHistoryIndex = -1;
+      e.preventDefault();
+      if ((historyIndex === -1 && e.key === 'ArrowDown') || (historyIndex === 0 && e.key === 'ArrowUp')) return;
+      if (e.key === 'ArrowUp' && historyIndex === -1) newHistoryIndex = commandHistory.length - 1;
+      else if (e.key === 'ArrowUp') newHistoryIndex -= 1;
+      if (e.key === 'ArrowDown' && historyIndex === commandHistory.length - 1) newHistoryIndex = -1;
+      else if (e.key === 'ArrowDown') newHistoryIndex += 1;
+      if (newHistoryIndex === -1) {
         e.currentTarget.value = '';
         setValue('');
-        animateCaret({ top: 0, left: caretStart });
+        setHistoryIndex(-1);
         return;
-      } else return;
-      const newCommand = commandHistory[historyIndex];
+      }
+      const newCommand = commandHistory[newHistoryIndex];
       e.currentTarget.value = newCommand;
-      e.currentTarget.selectionStart = newCommand.length;
+      e.currentTarget.setSelectionRange(e.currentTarget.value.length, e.currentTarget.value.length);
       setValue(newCommand);
       setHistoryIndex(newHistoryIndex);
-      animateCaret({ top: 0, left: caretStart + newCommand.length * fontWidth });
+    }
+    if (e.key === 'Tab' || e.key === 'ArrowRight' && toolTip) {
+      e.preventDefault();
+      if (toolTip) {
+        e.currentTarget.value = toolTip;
+        setValue(toolTip);
+      }
     }
   }
 })
