4cb3c23139d821ca3b2f60c5b0625318c81db8e7
MTLKS <matthew.liew99@gmail.com>
2023-05-01 10:35:13 +0000

    Add more queues and handle connect / disconnect

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -1,47 +1,71 @@
-import { WebSocketGateway, SubscribeMessage, ConnectedSocket, MessageBody, WebSocketServer, OnGatewayConnection } from "@nestjs/websockets";
-import { GameService } from "./game.service";
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  ConnectedSocket,
+  MessageBody,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { GameService } from './game.service';
 import { Socket, Server } from 'socket.io';
 
-@WebSocketGateway({ cors : {origin: '*'}, namespace: 'game'})
-export class GameGateway implements OnGatewayConnection{
-	constructor (private readonly gameService: GameService) {}
+@WebSocketGateway({ cors: { origin: '*' }, namespace: 'game' })
+export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly gameService: GameService) {}
 
-	@WebSocketServer()
-	server: Server;
+  @WebSocketServer()
+  server: Server;
 
-	handleConnection(client: Socket, ...args: any[]) {
-		// console.log("Client connected", client.id);
-		this.server.emit("test", "BOO");
-	}
+  handleConnection(@ConnectedSocket() client: Socket) {
+    this.gameService.handleConnection(client);
+  }
 
-	@SubscribeMessage('joinQueue')
-	async handleJoinQueue(@ConnectedSocket() client: Socket) {
-		this.gameService.joinQueue(client, this.server);
-	}
+  handleDisconnect(@ConnectedSocket() client: Socket) {
+    this.gameService.handleDisconnect(client);
+  }
 
-	@SubscribeMessage('leaveQueue')
-	async handleLeaveQueue(@ConnectedSocket() client: Socket) {
-		this.gameService.leaveQueue(client);
-	}
+  @SubscribeMessage('joinQueue')
+  async handleJoinQueue(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() body: any,
+  ) {
+    this.gameService.joinQueue(client, body.queue, this.server);
+  }
 
-	@SubscribeMessage('startGame')
-	async startGame(@ConnectedSocket() player1: Socket, @ConnectedSocket() player2: Socket, @MessageBody() body: any){
-		// const BODY = JSON.parse(body);
-		const BODY = body;
+  @SubscribeMessage('leaveQueue')
+  async handleLeaveQueue(@ConnectedSocket() client: Socket) {
+    this.gameService.leaveQueue(client);
+  }
 
-		//May send info such as clientID, player name through socket.data
-		// console.log(player1.data);
-		await this.gameService.startGame(player1, player2, this.server);
-	}
+  @SubscribeMessage('startGame')
+  async startGame(
+    @ConnectedSocket() player1: Socket,
+    @ConnectedSocket() player2: Socket,
+    @MessageBody() body: any,
+  ) {
+    // const BODY = JSON.parse(body);
+    const BODY = body;
 
-	@SubscribeMessage('gameEnd')
-	async gameEnd(@ConnectedSocket() player1: Socket, @ConnectedSocket() player2: Socket){
-		await this.gameService.gameEnd(player1, player2);
-	}
+    //May send info such as clientID, player name through socket.data
+    // console.log(player1.data);
+    await this.gameService.startGame(player1, player2, this.server);
+  }
 
-	@SubscribeMessage('playerMove')
-	async handleKeyDown(@ConnectedSocket() client: Socket, @MessageBody() body: any){
-		// console.log(body);
-		this.gameService.playerUpdate(client.id, body.y);
-	}
+  @SubscribeMessage('gameEnd')
+  async gameEnd(
+    @ConnectedSocket() player1: Socket,
+    @ConnectedSocket() player2: Socket,
+  ) {
+    await this.gameService.gameEnd(player1, player2);
+  }
+
+  @SubscribeMessage('playerMove')
+  async handleKeyDown(
+    @ConnectedSocket() client: Socket,
+    @MessageBody() body: any,
+  ) {
+    // console.log(body);
+    this.gameService.playerUpdate(client.id, body.y);
+  }
 }
