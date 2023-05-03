3b663a0ff0d287a6c663cf8ca9ae57b154caf45c
ijon <itan@student.42kl.edu.my>
2023-05-03 16:38:34 +0800

    fix join twice issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App'
 import './styles/index.css'
 import { AppProvider, createRoot, Text } from '@pixi/react';
 import { Application } from 'pixi.js';
-import GameStage, { GameTickCtx } from './game/GameStage';
+import GameStage, { gameTick, GameTickCtx } from './game/GameStage';
 import Game from './game/Game';
 
 import { GameTick } from './game/gameTick';
@@ -17,14 +17,12 @@ reactRoot.render(
   // </React.StrictMode>,
 )
 
-
-const gameTick = new GameTick();
-
 const pixiApp = new Application({
   width: 1600,
   height: 900,
   backgroundColor: 0x242424,
   view: document.getElementById('pixi') as HTMLCanvasElement,
+  antialias: true,
 });
 window.addEventListener('mousemove', (e) => {
   gameTick.updatePlayerPosition(e.clientY);
