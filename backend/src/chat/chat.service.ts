93d11177f03ea62ee4f3767da1455cf833ecde70
Sean Chuah <chuahtseyung2002@gmail.com>
2023-05-08 14:57:55 +0800

    Send more info when receiving message

--- backend/src/chat/chat.service.ts
+++ backend/src/chat/chat.service.ts
@@ -40,8 +40,8 @@ export class ChatService {
 		const FRIENDSHIP = [...await this.friendshipRepository.find({ where: {senderIntraName: USER_DATA.intraName, receiverIntraName: intraName} }), ...await this.friendshipRepository.find({ where: {senderIntraName: intraName, receiverIntraName: USER_DATA.intraName} })];
 		if (FRIENDSHIP.length === 0 || FRIENDSHIP[0].status !== "ACCEPTED")
 			return {"error": "Invalid friendhsip - You are not friends with this user"};
-		await this.messageRepository.save(new Message(USER_DATA.intraName, CHANNEL[0].channelId, true, message, new Date().toISOString()));
-		server.to(CHANNEL[0].channelId).emit("message", message);
+		await this.messageRepository.save(new Message(USER_DATA.intraName, CHANNEL[0].channelId, false, message, new Date().toISOString()));
+		server.to(CHANNEL[0].channelId).emit("message", { intraName: USER_DATA.intraName, message: message } );
 	}
 
 	// async createNewRoom(accessToken: string, roomName: string, isPrivate: boolean, password: string): Promise<any> {
