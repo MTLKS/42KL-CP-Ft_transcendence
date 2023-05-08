5890ee2d9f2db6506f44d020af9a14d400a333dc
ijon <itan@student.42kl.edu.my>
2023-05-08 19:30:16 +0800

    added gameEntities

---
+++ frontend/src/model/GameEntities.ts
@@ -0,0 +1,65 @@
+interface GameEntityData {
+  x?: number;
+  y?: number;
+  vx?: number;
+  vy?: number;
+  w?: number;
+  h?: number;
+}
+
+class GameEntity {
+  x: number;
+  y: number;
+  vx: number;
+  vy: number;
+  w: number;
+  h: number;
+  constructor({ x, y, vx, vy, w, h }: GameEntityData) {
+    this.x = x ?? 0;
+    this.y = y ?? 0;
+    this.vx = vx ?? 0;
+    this.vy = vy ?? 0;
+    this.w = w ?? 0;
+    this.h = h ?? 0;
+  }
+}
+
+class GameBlackhole extends GameEntity {
+  magnitude: number;
+  constructor({ x, y, vx, vy, w, h }: GameEntityData, magnitude: number) {
+    super({ x, y, vx, vy, w, h });
+    this.magnitude = magnitude;
+  }
+}
+
+class GameTimeZone extends GameEntity {
+  speedFactor: number;
+  constructor({ x, y, vx, vy, w, h }: GameEntityData, speedFactor: number) {
+    super({ x, y, vx, vy, w, h });
+    this.speedFactor = speedFactor ?? 1;
+  }
+}
+
+class GameBlock extends GameEntity {
+  compressionSpeedX: number;
+  compressionSpeedY: number;
+  constructor({ x, y, vx, vy, w, h }: GameEntityData) {
+    super({ x, y, vx, vy, w, h });
+    this.compressionSpeedX = 0;
+    this.compressionSpeedY = 0;
+  }
+
+  compressionDecay() {
+    this.w += this.compressionSpeedX;
+    this.h += this.compressionSpeedY;
+    if (this.compressionSpeedX > 0) this.compressionSpeedY += 0.1;
+    else this.compressionSpeedY -= 0.1;
+    if (this.compressionSpeedY > 0) this.compressionSpeedX += 0.1;
+    else this.compressionSpeedX -= 0.1;
+    this.compressionSpeedX *= 0.9;
+    this.compressionSpeedY *= 0.9;
+  }
+}
+
+export { GameBlackhole, GameTimeZone, GameBlock };
+export default GameEntity;
