550d33cc20ec2ce933c96053cd34ba04c22baac8
Sean Chuah <chuahtseyung2002@gmail.com>
2023-05-09 15:52:02 +0800

    change reset to use userName instead of intraName

--- frontend/src/pages/UserForm/UserFormName.tsx
+++ frontend/src/pages/UserForm/UserFormName.tsx
@@ -9,7 +9,7 @@ interface UserFormNameProps {
 
 function UserFormName(props: UserFormNameProps) {
 
-  const [userName, setUsername] = useState(props.user.intraName);
+  const [userName, setUsername] = useState(props.user.userName);
 
   return (
     <div className="flex flex-col gap-2">
