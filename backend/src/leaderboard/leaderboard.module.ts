3126a5570df7a2b01963ccdf2ccdadba2186fa52
MTLKS <matthew.liew99@gmail.com>
2023-05-17 14:24:49 +0000

    fix users entity

--- backend/src/leaderboard/leaderboard.module.ts
+++ backend/src/leaderboard/leaderboard.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { LeaderboardService } from './leaderboard.service';
 import { LeaderboardController } from './leaderboard.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entity/user.entity';
+import { User } from '../entity/users.entity';
 
 @Module({
 	imports: [TypeOrmModule.forFeature([User])],
