2eaad3f4e33d2d504207c24f1473ecf78c76ec07
Sean Chuah <chuahtseyung2002@gmail.com>
2023-05-18 11:36:54 +0800

    Done Swagger for App

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Headers } from '@nestjs/common';
-import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiHeader, ApiOkResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth/auth.service';
 import { AppDTO } from './dto/app.dto';
 
@@ -8,7 +8,8 @@ import { AppDTO } from './dto/app.dto';
 export class AppController {
 	constructor(private readonly authService: AuthService) {}
 
-	@ApiOkResponse({ type: AppDTO })
+	@ApiOkResponse({ description: "Used to initiate the login process for a user", type: AppDTO })
+	@ApiHeader({ name: 'Authorization', description: 'The encrypted access token of the user', required: true })
 	@Get()
 	startLogin(@Headers('Authorization') accessToken: string): Promise<AppDTO> {
 		return this.authService.startLogin(accessToken);
--- backend/src/dto/app.dto.ts
+++ backend/src/dto/app.dto.ts
@@ -1,6 +1,6 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class AppDTO {
-	@ApiPropertyOptional({ example: "https://google.com" })
-	redirectUrl?: string;
+	@ApiProperty({ example: "http://localhost:3000" })
+	redirectUrl: string;
 }
