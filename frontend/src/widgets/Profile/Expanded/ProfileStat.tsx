9a8fe0235dfcf2c65e2949dcfdec4e3b9a576601
ijon <ijontan9@gmail.com>
2023-05-30 00:57:02 +0800

    done recent match and  stat

--- frontend/src/widgets/Profile/Expanded/ProfileStat.tsx
+++ frontend/src/widgets/Profile/Expanded/ProfileStat.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import PreviewProfileContext from '../../../contexts/PreviewProfileContext';
+import { UserStats } from '../../../model/UserStats';
+import { getProfileStat } from '../../../api/profileAPI';
 
 interface ProfileStatProps {
   expanded: boolean;
@@ -6,13 +9,22 @@ interface ProfileStatProps {
 
 function ProfileStat(props: ProfileStatProps) {
   const { expanded } = props;
+  const { currentPreviewProfile } = useContext(PreviewProfileContext);
+  const [stats, setStats] = useState<UserStats | null>(null);
+
+  useEffect(() => {
+    getProfileStat(currentPreviewProfile.intraName).then((data) => {
+      setStats(data.data);
+    });
+  }, [currentPreviewProfile.intraName]);
+
 
   return (
     <div className='flex flex-col aspect-square transition-all duration-1000 ease-in-out box-border overflow-hidden'
       style={expanded ? { flex: '1 1 0%' } : { width: "0px" }}>
       <div className='bg-dimshadow h-full flex flex-col justify-center items-center text-highlight font-bold text-xs xl:text-sm overflow-hidden'>
-        <div>Win: 20</div>
-        <div>lose: 20</div>
+        <div>Win: {stats?.win}</div>
+        <div>lose: {stats?.lose}</div>
         <div
           className='hover:underline truncate'
           onMouseOver={() => console.log(`show my worst nightmare`)}
