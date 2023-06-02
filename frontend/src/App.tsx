bc74b3b5e776b0a17ea1a499b461f1cf4e34fc5c
ijon <itan@student.42kl.edu.my>
2023-06-02 23:24:38 +0800

    updated login logic to not display login page when refresh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,17 +12,20 @@ function App() {
   const [newUser, setNewUser] = useState(false);
   const [userData, setUserData] = useState<UserData>({} as UserData);
   const [updateUser, setUpdateUser] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     checkIfLoggedIn();
   }, []);
 
-  let page = <Login />;
+  let page = <></>;
   if (newUser || updateUser) {
     page = <UserForm userData={userData} isUpdatingUser={updateUser} setIsUpdatingUser={setUpdateUser} />;
   }
   else if (logged) {
     page = <HomePage setUserData={setUserData} setUpdateUser={setUpdateUser} userData={userData} />;
+  } else if (loaded) {
+    page = <Login />;
   }
 
   return (
@@ -31,43 +34,34 @@ function App() {
     </PolkaDotContainer>
   )
 
-  function checkIfLoggedIn() {
-    let loggin = false;
-
-    getMyProfile().then((res) => {
-      if ((res.data as UserData).accessToken) {
-        setUserData(res.data as UserData);
-        setLogged(true);
-        loggin = true;
-      }
-    }).catch((err) => {
-      setLogged(false);
-    });
-
-    if (loggin) return;
+  async function checkIfLoggedIn() {
+    const getProfileRes = await getMyProfile().catch((err) => { });
+    if (getProfileRes != null && (getProfileRes.data as UserData).accessToken) {
+      setUserData(getProfileRes.data as UserData);
+      setLogged(true);
+      return;
+    }
 
     const queryString: string = window.location.search;
     const urlParams: URLSearchParams = new URLSearchParams(queryString);
     let code: { code: string | null } = { code: urlParams.get('code') };
-    
+
     if (code.code) {
-      checkAuth(code.code).then(async (res) => {
-        if (res) {
-          localStorage.setItem('Authorization', (res as any).data.accessToken);
-          if ((res as any).data.accessToken) {
-            document.cookie = `Authorization=${(res as any).data.accessToken};`;
-          }
-          if ((res as any).data.newUser) {
-            setUserData((await getMyProfile()).data as UserData);
-            setNewUser(true);
-          } else {
-            login();
-          }
+      const res = await checkAuth(code.code);
+      if (res) {
+        if ((res as any).data.accessToken) {
+          document.cookie = `Authorization=${(res as any).data.accessToken};`;
         }
-      }).catch((err) => {
-        console.log(err);
-      });
+        if ((res as any).data.newUser) {
+          setUserData((await getMyProfile()).data as UserData);
+          setNewUser(true);
+        } else {
+          login();
+        }
+        return;
+      }
     }
+    setLoaded(true);
   }
 }
 
