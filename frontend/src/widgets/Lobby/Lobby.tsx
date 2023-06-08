c6d772a9f92a306d5c13b56ca953ee37a48d09de
Sean Chuah <chuahtseyung2002@gmail.com>
2023-06-08 22:16:36 +0800

    gamemode GIF DONE

--- frontend/src/widgets/Lobby/Lobby.tsx
+++ frontend/src/widgets/Lobby/Lobby.tsx
@@ -12,7 +12,9 @@ import longGIF from '../../../assets/GIFS/LongPaddle.gif'
 import longPNG from '../../../assets/GIFS/LongPaddle.png'
 import magnetPNG from '../../../assets/GIFS/MagnetPaddle.png'
 import magnetGIF from '../../../assets/GIFS/MagnetPaddle.gif'
+import boringGIF from '../../../assets/GIFS/BoringGame.gif'
 import standardGIF from '../../../assets/GIFS/StandardGame.gif'
+import deathGIF from '../../../assets/GIFS/DeathGame.gif'
 import { Active } from '../../../../backend/src/entity/active.entity';
 import { PaddleType } from '../../game/gameData'
 import { gameData } from '../../main'
@@ -324,7 +326,7 @@ function LobbyButton(props: LobbyButtonProps) {
       <p className={`uppercase font-extrabold text-lg ${text} ${active && "group-hover:text-dimshadow"} text-center`} style={{ fontSize: "25px" }}>{title}</p>
       <div ref={hoverRef} className={`z-10 pointer-events-none font-jbmono rounded-lg border-highlight border-4 text-start bg-dimshadow absolute w-[400px] h-[228px] transition-opacity ease-in duration-200 ${hover ? " opacity-100" : " opacity-0"} `}>
         {imgLoaded ? null : <div className='w-full h-full flex justify-center rounded-[4px] items-center animate-pulse bg-highlight/50' />}
-        <img src={title === 'boring' ? standardGIF : title === 'standard' ? standardGIF : standardGIF} height={200} width={400} className={` bg-clip-content rounded-[4px] ${imgLoaded ? "" : "hidden"}`} onLoad={() => imgOnLoad()} />
+        <img src={title === 'boring' ? boringGIF : title === 'standard' ? standardGIF : deathGIF} height={200} width={400} className={` bg-clip-content rounded-[4px] ${imgLoaded ? "" : "hidden"}`} onLoad={() => imgOnLoad()} />
       </div>
     </button>
   )
